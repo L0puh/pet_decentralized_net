ba3fcb9f973124b6aee62095e527800d196bafd3
Lopuh <1opuhart@gmail.com>
2023-07-27 14:38:37 +0900

    switched to datagram sockets

--- main.cpp
+++ main.cpp
@@ -15,12 +15,10 @@
 #include <errno.h>
 #include <stdio.h>
 #include <arpa/inet.h>
-
-const int BACK_LOG = 10;
+#include <mutex>
 
 struct Conn_t {
     int id;
-    int sockfd;
     uint16_t port;
     
 };
@@ -34,57 +32,39 @@ class Node {
     private:
         int id = 0;
         int sockfd;
+        std::mutex mtx;
         std::vector<Conn_t> connections;
     public:
         Node(std::string port);
         ~Node();
     public:
-        struct addrinfo* init_servinfo(std::string port);
-        void init_node(std::string port); 
+        int init_socket(std::string port);
         void handle_error(int result);
         void connections_print();
     public:
-        void handle_send(uint16_t port);
         void handle_command(std::string message, uint16_t port_me);
         std::string message_parse(std::string message);
         int connect_to(std::string port);
         std::string message_get();
     public:
-
         void send_to(std::string message, uint16_t port_me, uint16_t port_to);
         void send_all(std::string message, uint16_t port_me);
     public:
-        void handle_recv(int sockfd, const sockaddr_in *addr);
+        void handle_send(uint16_t port, int sockfd);
+        void handle_recv(int sockfd);
 };
 
-struct addrinfo* Node::init_servinfo(std::string port){
-    struct addrinfo hints, *servinfo;
-
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET;  
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
-
-    int res = getaddrinfo(NULL, port.c_str(), &hints, &servinfo);
-
-    if (res == -1){
-        fprintf(stderr, "error in getaddrinfo: %s\n", gai_strerror(res));
-        exit(1);
-    }
-    return servinfo;
-}
-void Node::init_node(std::string port){ 
+int Node::init_socket(std::string port){ 
     
-    struct addrinfo *servinfo = init_servinfo(port);
-    
-    sockfd = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-    handle_error(bind(sockfd, servinfo->ai_addr, servinfo->ai_addrlen) == -1);
-    freeaddrinfo(servinfo);
-    
-    handle_error(listen(sockfd, BACK_LOG) == -1);
+    struct sockaddr_in servinfo;  
+    servinfo.sin_family = AF_INET;
+    servinfo.sin_port = htons(stoi(port));
 
-    printf("server's up\n");
-    return;
+    sockfd = socket(servinfo.sin_family, SOCK_DGRAM, 0);
+    handle_error(bind(sockfd, (const struct sockaddr *)&servinfo, sizeof(servinfo)) == -1);
+    
+    printf("[port: %s] ready to chat\n", port.c_str() );
+    return sockfd;
 }
 void Node::handle_error(int result){
     if (result == -1){
@@ -92,68 +72,57 @@ void Node::handle_error(int result){
         exit(1);
     }
 }
-Node::Node(std::string port) {
-    init_node(port);
+Node::Node(std::string port){
+    int sockfd = init_socket(port);
 
-    struct sockaddr_in their_addr;
-    socklen_t their_addrlen = sizeof(their_addr);
     uint16_t u_port = stoi(port);
-    std::thread send_th(&Node::handle_send, this, u_port);
+
+    std::thread send_th(&Node::handle_send, this, u_port, sockfd);
     send_th.detach();
-    while(true){
-        int sockfd_new = accept(sockfd, (sockaddr*)&their_addr, &their_addrlen);
-        Conn_t conn{.id = id, .sockfd = sockfd_new}; 
-        id++;
-        connections.push_back(conn);
-        std::thread recv_th(&Node::handle_recv, this, sockfd_new, &their_addr);
-        recv_th.detach();
+    handle_recv(sockfd);
+
+}
+
+void Node::handle_recv(int sockfd){
+
+    int bytes;
+    Package pckg;
+    struct sockaddr_in their_addr;
+
+    memset(&their_addr, 0, sizeof(their_addr));
+    socklen_t their_addrlen = sizeof(their_addr);
+
+    while ((bytes = recvfrom(sockfd, &pckg, sizeof(pckg), 0, (struct sockaddr *)&their_addr, &their_addrlen)) != -1 ){
+
+        char *buff = new char[pckg.message_size + 1];
+        buff[pckg.message_size] = '\0';
+        handle_error(recvfrom(sockfd, buff, pckg.message_size, 0, (struct sockaddr *)&their_addr, &their_addrlen) > 0);
+
+        printf("[%s:%d]: %s\n", inet_ntoa(their_addr.sin_addr), pckg.from, buff);
+        delete[] buff;
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            connect_to(std::to_string(pckg.from));               
+            memset(&their_addr, 0, sizeof(their_addr));
+        } 
     }
+    handle_error(bytes);
 }
 Node::~Node(){
     close(sockfd);
     exit(1);
-}
-void Node::handle_recv(int cur_sockfd, const sockaddr_in *addr){
-    int bytes, pckg_size;
-    Package pckg; 
-    
-    while((bytes = recv(cur_sockfd, &pckg, sizeof(pckg), 0)) > 0){
-        char *buff = new char[pckg.message_size + 1];
-        buff[pckg.message_size] = '\0';
-        handle_error(recv(cur_sockfd, buff, pckg.message_size, 0));
-        printf("[%s:%d]: %s\n", inet_ntoa(addr->sin_addr), pckg.from, buff);
-        delete[] buff;
-        for(std::vector<Conn_t>::iterator itr = connections.begin(); itr != connections.end(); itr++){
-            if (itr->sockfd == cur_sockfd && itr->port != pckg.from) {
-                itr->port = pckg.from;
-                close(cur_sockfd);
-                std::string port_str = std::to_string(pckg.from);
-                connect_to(port_str);
-                return;
-            } 
-        }
-    }
-    handle_error(bytes);
-    if (bytes == 0 ){
-        printf("user's disconnected\n");
-        close(cur_sockfd);
-        
-        //TODO: delete user from connections
-    }
-    
 }
 
 void Node::connections_print(){
     printf("[+] your connections: \n");
     for (std::vector<Conn_t>::iterator itr = connections.begin();
             itr != connections.end(); itr++){
-        printf("%d : %hu | %d \n", itr->id, itr->port, itr->sockfd);
+        printf("%d : %hu \n", itr->id, itr->port);
     }
 
 }
 void Node::handle_command(std::string message, uint16_t port_me){
     int i=0;
-    uint16_t port_to;
     if (message == "/quit") exit(0);
     else if(message == "/show") connections_print();
     else {
@@ -163,21 +132,16 @@ void Node::handle_command(std::string message, uint16_t port_me){
         i=0;
         if (msg == "/connect"){
             std::string port = message_parse(message);
-            sockfd = connect_to(port);
-            port_to = stoi(port);
-            Conn_t new_conn{.id = id, .sockfd = sockfd, .port = port_to};
-            connections.push_back(new_conn);
-            id++;
+            connect_to(port);
         } else if(msg == "/send"){
             std::string port = message_parse(message);
-            sockfd = connect_to(port);
-            send_to(message, port_me, port_to);
+            connect_to(port);
+            /* send_to(message, port_me, port_to); */
         }
 
     }
 }
-void Node::handle_send(uint16_t port){
-    int sockfd=0;
+void Node::handle_send(uint16_t port, int sockfd){
     Package pckg;
     while (true) {
         std::string message = message_get();
@@ -195,28 +159,41 @@ void Node::send_to(std::string message, uint16_t port_me, uint16_t port_to){
 }
 void Node::send_all(std::string message, uint16_t port_me){
     Package pckg {.from=port_me, .message_size = message.size()};
-
-
+    struct sockaddr_in their_addr;
+    memset(&their_addr, 0, sizeof(their_addr));
+    their_addr.sin_family = AF_INET;
+        
+    socklen_t their_addrlen = sizeof(their_addr);
+    
     for (std::vector<Conn_t>::iterator itr = connections.begin();
         itr != connections.end(); itr++){
             pckg.to = itr->port;
-            handle_error(send(itr->sockfd, &pckg, sizeof(pckg), 0));
-            handle_error(send(itr->sockfd, message.c_str(), message.size(), 0));
+            their_addr.sin_port = htons(itr->port);
+            handle_error(sendto(sockfd, &pckg, sizeof(pckg), 0, (const struct sockaddr*)&their_addr, their_addrlen));
+            handle_error(sendto(sockfd, message.c_str(), message.size(), 0, (const struct sockaddr*)&their_addr, their_addrlen));
          }
+
     return;
 }
 
 
 int Node::connect_to(std::string port){
-    struct sockaddr_in addr; 
-    
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(stoi(port));
-
-    int conn = socket(AF_INET, SOCK_STREAM, 0);
-    handle_error(connect(conn, (struct sockaddr*)&addr, sizeof(addr)));
-    printf("connected to port: %s\n", port.c_str());
-    return conn;
+    uint16_t u_port = stoi(port);
+    bool exsist = false;
+    std::vector<Conn_t>::iterator itr = connections.begin();
+    while(itr != connections.end()){
+        itr++;
+        if (itr->port == u_port){
+            exsist=true;
+            break;
+        } 
+    }
+    if (!exsist || connections.size() == 0)  {
+        connections.push_back(Conn_t{.id=id, .port=u_port});
+        id++;
+        printf("connected to port: %s\n", port.c_str());
+    }
+    return 0;
 
 }
 std::string Node::message_parse(std::string message){
@@ -238,6 +215,5 @@ int main (int argc, char* argv[]) {
     }
     std::string port = argv[1];
     port.erase(port.begin());
-    printf("port: %s\n", port.c_str());
     Node node(port);
 }
